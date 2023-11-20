c55cb27b33619cff499a5c1f54756afb0e0e926c
ravi544747 <ravi544747@gmail.com>
2023-11-20 21:00:19 +0530

    this is our 1 version of code

---
+++ data_dump.py
@@ -0,0 +1,23 @@
+import pymongo
+import pandas as pd
+import json
+# provide then local host url to connect the python to mongo db
+client=pymongo.MongoClient('mongodb://localhost:27017')
+
+#assigning the name for the database and collection:
+DATA_FILE_PATH='/config/workspace/aps_failure_training_set1.csv'
+DATABASE_NAME="aps"
+COLLECTION_NAME='sensor'
+
+if __name__=="__main__":
+    df=pd.read_csv(DATA_FILE_PATH)
+    print(f'ROws and columns{df.shape}')
+
+# convert the data frame to the Jason format , sotht we can dump the record in the mango DB
+
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+    # insert converted jason record to mongo DB
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
